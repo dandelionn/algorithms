b2e29e5fded7a22380ed517c653cb93a7f56e66f
DESKTOP-7BP57RP\Paul <michea.paul@yahoo.com>
2020-11-05 15:08:35 +0200

    removed Arbori folder

--- Arbori/binary trees/tree traversals/dynamic/custom_type.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <memory>
-
-struct Node{
-    int value; 
-    std::unique_ptr<Node> left;
-    std::unique_ptr<Node> right;
-};
--- Arbori/rooted trees/tree_traversal.h
+++
@@ -1,7 +0,0 @@
-#include <vector>
-#include <stack>
-#include <queue>
-
-std::vector<int> BreadthFirst(const std::vector<std::vector<int>>& tree, int root);
-std::vector<int> DepthFirst(const std::vector<std::vector<int>>& tree, int root);
-std::vector<int> DepthFirst_iterative(const std::vector<std::vector<int>>& tree, int root);
